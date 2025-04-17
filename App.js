1f3c22a8f8dd895f48cef9cdca249823a9a71782
Dani Canteros <danielcanteros1998@gmail.com>
2025-04-17 09:16:53 -0300

    Agregado 10

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const icon = require('./assets/icon.png');
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image source={icon} style={{ width: 100, height: 100 }} />
+      <Text>Hola!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}); 
+
+
+
